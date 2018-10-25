43b809d94db434e54368b76b7b2ff9e7e3a1187e
David4FSU <David4FSU@hotmail.com>
2018-10-25 14:41:25 -0500

    Added Titles to the Markers

--- src/components/Map.js
+++ src/components/Map.js
@@ -7,12 +7,12 @@ const MyMapComponent = withScriptjs(withGoogleMap((props) =>
         defaultZoom={14}
         defaultCenter={{ lat: 33.056146, lng: -97.065747 }}
     >
-        {props.isMarkerShown && <Marker position={{ lat: 33.065145, lng: -97.080881 } } /> } {/*Starwood*/}
-        {props.isMarkerShown && <Marker position={{ lat: 33.072347, lng: -97.059849 } } /> } {/*Seven Mile Cafe*/}
-        {props.isMarkerShown && <Marker position={{ lat: 33.047154, lng: -97.070295 } } /> } {/*Mi Dia from Scratch*/}
-        {props.isMarkerShown && <Marker position={{ lat: 33.073760, lng: -97.063936 } } /> } {/*Frescos*/}
-        {props.isMarkerShown && <Marker position={{ lat: 33.074307, lng: -97.068947 } } /> } {/*What's On Tap*/}
-        {props.isMarkerShown && <Marker position={{ lat: 33.033979, lng: -97.072304 } } /> } {/*Local Pint*/}
+        {props.isMarkerShown && <Marker title="Starwood" position={{ lat: 33.065145, lng: -97.080881 } } /> } 
+        {props.isMarkerShown && <Marker title="Seven Mile Cafe" position={{ lat: 33.072347, lng: -97.059849 } } /> } 
+        {props.isMarkerShown && <Marker title="Mi Dia from Scratch" position={{ lat: 33.047154, lng: -97.070295 } } /> } 
+        {props.isMarkerShown && <Marker title="Frescos" position={{ lat: 33.073760, lng: -97.063936 } } /> } 
+        {props.isMarkerShown && <Marker title="What's On Tap*" position={{ lat: 33.074307, lng: -97.068947 } } /> } 
+        {props.isMarkerShown && <Marker title="Local Pint" position={{ lat: 33.033979, lng: -97.072304 } } /> } 
 
     </GoogleMap>
 ))
