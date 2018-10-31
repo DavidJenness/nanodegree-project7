a6197d5f161ee4fc704882049591bccca5e3ea9e
David4FSU <David4FSU@hotmail.com>
2018-10-31 09:56:19 -0500

    Added Sidebar

--- src/components/MapDisplay.js
+++ src/components/MapDisplay.js
@@ -25,36 +25,45 @@ export class MapDisplay extends Component {
     }
   };
 
-
   render() {
     return (
-      <Map
-        google={this.props.google}
-        initialCenter={{
-          lat: 33.056146,
-          lng: -97.065747
-        }}
-        onClick={this.onMapClicked}
-      >
-        {this.props.locations.map(marker => (
-          <Marker
-            position={{ lat: marker.lat, lng: marker.lng }}
-            title={marker.name}
-            key={marker.uniqueID}
-            onClick={this.onMarkerClick}
-            name={marker.name}
-          />
-        ))}
+      <div>
+        <div className="pageContainer">
+        <div className="sidebar">
+        This is the sidebar.
+        </div>
+        
+        <div className="map" ref="map">
+          <Map
+            google={this.props.google}
+            initialCenter={{
+              lat: 33.056146,
+              lng: -97.065747
+            }}
+            onClick={this.onMapClicked}
+          >
+            {this.props.locations.map(marker => (
+              <Marker
+                position={{ lat: marker.lat, lng: marker.lng }}
+                title={marker.name}
+                key={marker.uniqueID}
+                onClick={this.onMarkerClick}
+                name={marker.name}
+              />
+            ))}
 
-        <InfoWindow
-          marker={this.state.activeMarker}
-          visible={this.state.showingInfoWindow}
-        >
-          <div>
-            <Information selectedPlace={this.state.selectedPlace}/>
-          </div>
-        </InfoWindow>
-      </Map>
+            <InfoWindow
+              marker={this.state.activeMarker}
+              visible={this.state.showingInfoWindow}
+            >
+              <div>
+                <Information selectedPlace={this.state.selectedPlace} />
+              </div>
+            </InfoWindow>
+          </Map>
+        </div>
+      </div>
+      </div>
     );
   }
 }
