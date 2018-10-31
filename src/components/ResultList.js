de5ab2fa7c00ecd778ad6757507694fab1d04145
David4FSU <David4FSU@hotmail.com>
2018-10-31 14:57:37 -0500

    working on search box

--- src/components/ResultList.js
+++ src/components/ResultList.js
@@ -1,17 +1,24 @@
-import React, { Component } from "react"
+import React, { Component } from "react";
 import ListItem from "./ListItem";
 
 export default class ResultList extends Component {
+  state = {
+    searchText: ""
+  };
+  handleChange = (event) => {
+      this.setState({searchText: event.target.value})
+  }
 
-    render() {
-        return <div>
-
-            {this.props.locations.map(myLocation => (
-                <ListItem myLocation={myLocation}/>
-            ))}
-
-    
-        </div>
-
-    }
+  render() {
+    return (
+      <div>
+        <form>
+          <input type="text" value={this.state.searchText} onChange={this.handleChange}/> 
+        </form>
+        {this.props.locations.map(myLocation => (
+          <ListItem myLocation={myLocation} />
+        ))}
+      </div>
+    );
+  }
 }
